185f493d883d5e2dbbce4289933bbdb0bc395b90
chitransh <chitranshjawre2001@gmail.com>
2026-03-10 13:38:00 +0530

    update

--- src/routes/StoreRoutes.jsx
+++ src/routes/StoreRoutes.jsx
@@ -1,21 +1,43 @@
-import React, { useEffect, useState } from "react";
+// import React, { useEffect, useState } from "react";
+// import { Navigate } from "react-router-dom";
+// import { verifySeller } from "../Api/authApi";
+
+// const SellerProtectedRoute = ({ children }) => {
+//   const [authenticated, setAuthenticated] = useState(null);
+
+//   useEffect(() => {
+//     verifySeller()
+//       .then(() => setAuthenticated(true))
+//       .catch(() => setAuthenticated(false));
+//   }, []);
+
+//   if (authenticated === null) return <div>Loading...</div>;
+
+//   if (!authenticated) {
+//     // return <Navigate to="/seller-login" replace />;
+//     return "SellerProtectedRoute: Access denied. Please log in as a seller to view this page.";
+//   }
+
+//   return children;
+// };
+
+// export default SellerProtectedRoute;
+
+
+
+import React from "react";
 import { Navigate } from "react-router-dom";
-import { verifySeller } from "../Api/authApi";
+import Cookies from "js-cookie";
 
 const SellerProtectedRoute = ({ children }) => {
-  const [authenticated, setAuthenticated] = useState(null);
+  const sellerToken = Cookies.get("sellerToken");
 
-  useEffect(() => {
-    verifySeller()
-      .then(() => setAuthenticated(true))
-      .catch(() => setAuthenticated(false));
-  }, []);
-
-  if (authenticated === null) return <div>Loading...</div>;
-
-  if (!authenticated) {
-    // return <Navigate to="/seller-login" replace />;
-    return "SellerProtectedRoute: Access denied. Please log in as a seller to view this page.";
+  if (!sellerToken) {
+    return (
+      <Navigate to="/seller-login" replace />
+      // or you can return custom message
+      // "Access denied. Please login as seller"
+    );
   }
 
   return children;
